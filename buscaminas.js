901727d0f564b62fc3401ba1850808825daac855
Unknown <witlesshydra@outlook.com>
2019-05-15 14:54:53 +0200

    functional

--- buscaminas.js
+++ buscaminas.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 require('dotenv').config();
+
 class buscaminas {
 
     constructor(alto, largo, minas){
@@ -8,6 +9,9 @@ class buscaminas {
         this.largo = largo;
         this.minas = minas;
         this.mapa = [];
+
+        this.constructorMinas();
+        this.nato();
     }
 
     constructorMinas(){
@@ -19,23 +23,73 @@ class buscaminas {
         var i=0;
         for (var x=0; x<this.alto; x++) {
             for (var y = 0; y < this.largo; y++) {
-                lista[i] = [x,y];
-                i++;
-                this.mapa[x][y]=0;
+                lista[x*this.alto+y] = [x,y];
+                this.mapa[x][y]=":one:";
             }
 
         }
         while(this.minas){
             var rand = Math.floor(Math.random() * lista.length);
-         /*   this.mapa[4][2] = 10;
-            console.log(this.mapa[4][2]); */
-            this.mapa[lista[rand][0]][lista[rand][1]] = 9;
+            this.mapa[lista[rand][0]][lista[rand][1]] = ":bomb:";
             console.log(this.mapa[lista[rand][0]][lista[rand][1]]);
             this.minas--;
             lista.splice(rand, 0);
         }
     }
-    ver(){
+
+    nato(){
+        var contador = 0;
+
+        for(var i=0; i<this.alto; i++){
+            for(var j=0; j<this.largo; j++){
+                if(this.mapa[i][j] !== ":bomb:"){
+                    for(var k=-1; k<2; k++){
+                        for(var l=-1; l<2; l++){
+                            if(i+k >= 0 && i+k < this.alto && j+l >=0 && j+l < this.largo){
+                                console.log(this.mapa);
+                                console.log("----------");
+                                if(this.mapa[i+k][j+l] === ":bomb:"){
+                                    contador++;
+                                }
+                            }
+                        }
+                    }
+                    switch(contador) {
+                        case 0:
+                            this.mapa[i][j] = ":zero:";
+                            break;
+                        case 1:
+                            this.mapa[i][j] = ":one:";
+                            break;
+                        case 2:
+                            this.mapa[i][j] = ":two:";
+                            break;
+                        case 3:
+                            this.mapa[i][j] = ":three:";
+                            break;
+                        case 4:
+                            this.mapa[i][j] = ":four:";
+                            break;
+                        case 5:
+                            this.mapa[i][j] = ":five:";
+                            break;
+                        case 6:
+                            this.mapa[i][j] = ":six:";
+                            break;
+                        case 7:
+                            this.mapa[i][j] = ":seven:";
+                            break;
+                        case 8:
+                            this.mapa[i][j] = ":eight:";
+                            break;
+                    }
+                    contador = 0;
+                }
+            }
+        }
+    }
+    ver(fila){
+        return "||" + this.mapa[fila].join("||||") + "||";
         for(var x=0; x<this.alto; x++){
             for(var y=0; y<this.largo; y++){
                 console.log(this.mapa[x][y]);
--- main.js
+++ main.js
@@ -1,20 +1,52 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const buscaminas = require('./buscaminas');
+const conf = require('./config.json');
+
+require('dotenv').config();
 
 client.on("ready", () => {
     client.user.setActivity("MINAS", { type: "WATCHING"})
 })
 
 client.on ("message", (message) => {
+
+    var msg = "";
+
+    if(message.author.bot){
+        return (0);
+    }
+
     if(message.content.startsWith("/create")){
         var params = message.content.split(" ");
+        if(isNaN(+params[1]) || isNaN(+params[2]) || isNaN(+params[3])){
+            message.channel.send(config.error1);
+            return (0);
+        }
+        if(+params[1] > conf.MAX_HEIGHT || +params[2] > conf.MAX_HEIGHT){
+            message.channel.send(config.error2);
+            return (0);
+        }
+        if(+params[3] > +params[1]*+params[2]){
+            message.channel.send(config.error3);
+            return (0);
+        }
+
         const bm = new buscaminas(params[1], params[2], params[3]);
-        var svid = message.guild.id;
-        var chid = message.channel.id;
-        bm.constructorMinas();
-        bm.ver();
-        message.channel.send("Ok");
+        for (var i = 0; i<params[1]; i++){
+            sleep(100);
+            msg+= bm.ver(i) + "\n";
+        }
+        message.channel.send(msg);
+    }
+
+    function sleep(ms) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > ms){
+                break;
+            }
+        }
     }
 });
 
